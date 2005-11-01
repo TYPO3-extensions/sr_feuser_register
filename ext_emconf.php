9215c416ec3ddd60e3c3cac6cecdbcd65bc6f6dc
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-01 05:15:39 +0000

    * Version 1.6.8 * Corrections to a few glitches introduced in version 1.6.7. * Update to the Russian translation. * New TS constant confirmInvitationPID.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register/trunk@2026 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sr_feuser_register"
 # 
-# Auto generated 28-10-2005 21:47
+# Auto generated 01-11-2005 00:04
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '3.5.3-0.0.3',
-	'PHP_version' => '0.0.23-0.0.23',
+	'TYPO3_version' => '3.5.4-0.0.4',
+	'PHP_version' => '0.0.24-0.0.24',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '1.6.7',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"6ccb";s:21:"ext_conf_template.txt";s:4:"dffc";s:12:"ext_icon.gif";s:4:"ad8f";s:17:"ext_localconf.php";s:4:"c01a";s:14:"ext_tables.php";s:4:"56fd";s:14:"ext_tables.sql";s:4:"ac59";s:28:"ext_typoscript_constants.txt";s:4:"47f9";s:28:"ext_typoscript_editorcfg.txt";s:4:"de89";s:24:"ext_typoscript_setup.txt";s:4:"fd87";s:13:"locallang.php";s:4:"fc52";s:16:"locallang_db.php";s:4:"64d5";s:7:"tca.php";s:4:"d55f";s:37:"pi1/class.tx_srfeuserregister_pi1.php";s:4:"4da2";s:48:"pi1/class.tx_srfeuserregister_pi1_adodb_time.php";s:4:"cdb8";s:52:"pi1/class.tx_srfeuserregister_pi1_t3lib_htmlmail.php";s:4:"6e3f";s:50:"pi1/class.tx_srfeuserregister_pi1_urlvalidator.php";s:4:"b2a7";s:19:"pi1/icon_delete.gif";s:4:"f914";s:17:"pi1/locallang.php";s:4:"7333";s:36:"pi1/tx_srfeuserregister_htmlmail.css";s:4:"7cfc";s:38:"pi1/tx_srfeuserregister_pi1_sample.txt";s:4:"297e";s:37:"pi1/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"7c11";s:14:"doc/manual.sxw";s:4:"ad45";s:48:"hooks/class.tx_srfeuserregister_hooksHandler.php";s:4:"ca8f";}',
+	'version' => '1.6.8',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"6ccb";s:21:"ext_conf_template.txt";s:4:"dffc";s:12:"ext_icon.gif";s:4:"ad8f";s:17:"ext_localconf.php";s:4:"c01a";s:14:"ext_tables.php";s:4:"d131";s:14:"ext_tables.sql";s:4:"ac59";s:28:"ext_typoscript_constants.txt";s:4:"d94e";s:28:"ext_typoscript_editorcfg.txt";s:4:"de89";s:24:"ext_typoscript_setup.txt";s:4:"8e6c";s:13:"locallang.php";s:4:"fc52";s:16:"locallang_db.php";s:4:"524b";s:7:"tca.php";s:4:"d55f";s:37:"pi1/class.tx_srfeuserregister_pi1.php";s:4:"6365";s:48:"pi1/class.tx_srfeuserregister_pi1_adodb_time.php";s:4:"cdb8";s:52:"pi1/class.tx_srfeuserregister_pi1_t3lib_htmlmail.php";s:4:"6e3f";s:50:"pi1/class.tx_srfeuserregister_pi1_urlvalidator.php";s:4:"b2a7";s:19:"pi1/icon_delete.gif";s:4:"f914";s:17:"pi1/locallang.php";s:4:"939c";s:36:"pi1/tx_srfeuserregister_htmlmail.css";s:4:"7cfc";s:38:"pi1/tx_srfeuserregister_pi1_sample.txt";s:4:"297e";s:37:"pi1/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"7922";s:14:"doc/manual.sxw";s:4:"b8c5";s:48:"hooks/class.tx_srfeuserregister_hooksHandler.php";s:4:"ca8f";}',
 );
 
 ?>
