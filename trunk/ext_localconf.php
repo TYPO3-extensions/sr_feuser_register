1c6fcae01459da23859cfc5181324f163cf23692
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-26 04:20:15 +0000

    * Added support for backend flexforms by Franz Holzinger.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register@2057 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_localconf.php
+++ trunk/ext_localconf.php
@@ -23,14 +23,17 @@ $_EXTCONF = unserialize($_EXTCONF);    // unserializing the configuration so we 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['uploadFolder'] = $_EXTCONF['uploadFolder'] ? $_EXTCONF['uploadFolder'] : 'uploads/tx_srfeuserregister';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['imageMaxSize'] = $_EXTCONF['imageMaxSize'] ? $_EXTCONF['imageMaxSize'] : 250;
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['imageTypes'] = $_EXTCONF['imageTypes'] ? $_EXTCONF['imageTypes'] : 'png,jpeg,jpg,gif,tif,tiff';
-	
+
 	/* Example of configuration of hooks
 $TYPO3_CONF_VARS['EXTCONF']['sr_feuser_register']['tx_srfeuserregister_pi1']['confirmRegistrationClass'][] = 'EXT:sr_feuser_register/hooks/class.tx_srfeuserregister_hooksHandler.php:&tx_srfeuserregister_hooksHandler';
 $TYPO3_CONF_VARS['EXTCONF']['sr_feuser_register']['tx_srfeuserregister_pi1']['registrationProcess'][] = 'EXT:sr_feuser_register/hooks/class.tx_srfeuserregister_hooksHandler.php:&tx_srfeuserregister_hooksHandler';
 	*/
 
 	// turn the use of flexforms on:
-//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['useFlexforms'] = $_EXTCONF['useFlexforms'] ? $_EXTCONF['useFlexforms'] : (t3lib_div::int_from_ver($GLOBALS['TYPO_VERSION'] >= 3007000 ));
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['useFlexforms'] = $_EXTCONF['useFlexforms'];
+if (t3lib_div::int_from_ver($GLOBALS['TYPO_VERSION']) < 3007000) {
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['useFlexforms'] = 0;
+}
 
 if (!defined ('FH_LIBRARY_EXTkey')) {
 	define('FH_LIBRARY_EXTkey','fh_library');
