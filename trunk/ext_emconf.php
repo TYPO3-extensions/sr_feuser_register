f60bbcf0ee8e36b58de9fcdc2deea8ef90484b12
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-01-13 01:37:33 +0000

    * Version 2.2.4: * Correction to the generateUsername property: when set, the username was erased on editing the profile. * For TCA-driven select fields, if no default value is set in TS template, apply default value from TCA, if present.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register@2065 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sr_feuser_register"
 # 
-# Auto generated 08-01-2006 16:30
+# Auto generated 10-01-2006 10:32
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '2.2.3',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"d98f";s:21:"ext_conf_template.txt";s:4:"984f";s:12:"ext_icon.gif";s:4:"ad8f";s:17:"ext_localconf.php";s:4:"a04e";s:14:"ext_tables.php";s:4:"ca1f";s:14:"ext_tables.sql";s:4:"13ca";s:13:"locallang.php";s:4:"fc52";s:16:"locallang_db.php";s:4:"a189";s:7:"tca.php";s:4:"1777";s:37:"pi1/class.tx_srfeuserregister_pi1.php";s:4:"731f";s:48:"pi1/class.tx_srfeuserregister_pi1_adodb_time.php";s:4:"cdb8";s:52:"pi1/class.tx_srfeuserregister_pi1_t3lib_htmlmail.php";s:4:"6e3f";s:50:"pi1/class.tx_srfeuserregister_pi1_urlvalidator.php";s:4:"b2a7";s:18:"pi1/ext_tables.php";s:4:"b0ed";s:23:"pi1/flexform_ds_pi1.xml";s:4:"bc95";s:19:"pi1/icon_delete.gif";s:4:"f914";s:21:"pi1/internal_link.gif";s:4:"12b9";s:32:"pi1/internal_link_new_window.gif";s:4:"402a";s:17:"pi1/locallang.php";s:4:"9f34";s:36:"pi1/tx_srfeuserregister_htmlmail.css";s:4:"0570";s:42:"pi1/tx_srfeuserregister_htmlmail_xhtml.css";s:4:"f65b";s:41:"pi1/tx_srfeuserregister_pi1_css_tmpl.html";s:4:"18c9";s:38:"pi1/tx_srfeuserregister_pi1_sample.txt";s:4:"297e";s:37:"pi1/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"a56b";s:14:"doc/manual.sxw";s:4:"5115";s:48:"hooks/class.tx_srfeuserregister_hooksHandler.php";s:4:"3d7e";s:28:"scripts/jsfunc.updateform.js";s:4:"e838";s:30:"static/old_style/constants.txt";s:4:"54fa";s:30:"static/old_style/editorcfg.txt";s:4:"cfad";s:26:"static/old_style/setup.txt";s:4:"b8f0";s:31:"static/css_styled/constants.txt";s:4:"10f2";s:27:"static/css_styled/setup.txt";s:4:"c92a";}',
+	'version' => '2.2.4',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"016a";s:21:"ext_conf_template.txt";s:4:"984f";s:12:"ext_icon.gif";s:4:"ad8f";s:17:"ext_localconf.php";s:4:"ffa9";s:14:"ext_tables.php";s:4:"ca1f";s:14:"ext_tables.sql";s:4:"13ca";s:13:"locallang.php";s:4:"fc52";s:16:"locallang_db.php";s:4:"a189";s:7:"tca.php";s:4:"1777";s:37:"pi1/class.tx_srfeuserregister_pi1.php";s:4:"25fc";s:48:"pi1/class.tx_srfeuserregister_pi1_adodb_time.php";s:4:"cdb8";s:52:"pi1/class.tx_srfeuserregister_pi1_t3lib_htmlmail.php";s:4:"6e3f";s:50:"pi1/class.tx_srfeuserregister_pi1_urlvalidator.php";s:4:"b2a7";s:18:"pi1/ext_tables.php";s:4:"b0ed";s:23:"pi1/flexform_ds_pi1.xml";s:4:"bc95";s:19:"pi1/icon_delete.gif";s:4:"f914";s:21:"pi1/internal_link.gif";s:4:"12b9";s:32:"pi1/internal_link_new_window.gif";s:4:"402a";s:17:"pi1/locallang.php";s:4:"9f34";s:36:"pi1/tx_srfeuserregister_htmlmail.css";s:4:"0570";s:42:"pi1/tx_srfeuserregister_htmlmail_xhtml.css";s:4:"f65b";s:41:"pi1/tx_srfeuserregister_pi1_css_tmpl.html";s:4:"18c9";s:38:"pi1/tx_srfeuserregister_pi1_sample.txt";s:4:"297e";s:37:"pi1/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"a56b";s:14:"doc/manual.sxw";s:4:"e4c8";s:48:"hooks/class.tx_srfeuserregister_hooksHandler.php";s:4:"3d7e";s:28:"scripts/jsfunc.updateform.js";s:4:"e838";s:30:"static/old_style/constants.txt";s:4:"54fa";s:30:"static/old_style/editorcfg.txt";s:4:"cfad";s:26:"static/old_style/setup.txt";s:4:"b8f0";s:31:"static/css_styled/constants.txt";s:4:"10f2";s:27:"static/css_styled/setup.txt";s:4:"c92a";}',
 );
 
 ?>
