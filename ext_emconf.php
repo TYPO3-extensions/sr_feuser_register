43024732e87743633fa5cfd72525d9d1eef6d95f
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-08-14 16:43:53 +0000

    * Version 2.5.1 * Update to Russian translation. * When a foreign table is used on a select field, sort values according to the sortby field. * Fix bugtracker issue 3992: Hook not working: confirmRegistrationClass_postProcess * Remove duplicate class attributes on multiple statements of the css-styles html template. * Remove deprecated wrap attribute from dynamically-generated textearea html statements. * Remove for attribute from label tag of "gender" radio buttons field. * onchange and onsubmit attribute names should be lowercase.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register/trunk@3596 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sr_feuser_register"
 #
-# Auto generated 21-06-2006 14:17
+# Auto generated 14-08-2006 12:39
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Fructifor Inc.',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '2.5.0',
-	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"adfb";s:21:"ext_conf_template.txt";s:4:"3a54";s:12:"ext_icon.gif";s:4:"ad8f";s:17:"ext_localconf.php";s:4:"9c0a";s:14:"ext_tables.php";s:4:"bdeb";s:14:"ext_tables.sql";s:4:"dc58";s:13:"locallang.xml";s:4:"f26f";s:16:"locallang_db.xml";s:4:"cd07";s:7:"tca.php";s:4:"aad1";s:37:"pi1/class.tx_srfeuserregister_pi1.php";s:4:"eedf";s:48:"pi1/class.tx_srfeuserregister_pi1_adodb_time.php";s:4:"cdb8";s:50:"pi1/class.tx_srfeuserregister_pi1_urlvalidator.php";s:4:"b2a7";s:18:"pi1/ext_tables.php";s:4:"b0ed";s:23:"pi1/flexform_ds_pi1.xml";s:4:"bc95";s:19:"pi1/icon_delete.gif";s:4:"f914";s:21:"pi1/internal_link.gif";s:4:"12b9";s:32:"pi1/internal_link_new_window.gif";s:4:"402a";s:17:"pi1/locallang.xml";s:4:"c019";s:36:"pi1/tx_srfeuserregister_htmlmail.css";s:4:"0570";s:42:"pi1/tx_srfeuserregister_htmlmail_xhtml.css";s:4:"f65b";s:41:"pi1/tx_srfeuserregister_pi1_css_tmpl.html";s:4:"fb70";s:38:"pi1/tx_srfeuserregister_pi1_sample.txt";s:4:"297e";s:37:"pi1/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"95d3";s:14:"doc/manual.sxw";s:4:"8a5e";s:48:"hooks/class.tx_srfeuserregister_hooksHandler.php";s:4:"3d7e";s:28:"scripts/jsfunc.updateform.js";s:4:"e838";s:30:"static/old_style/constants.txt";s:4:"4bde";s:30:"static/old_style/editorcfg.txt";s:4:"cfad";s:26:"static/old_style/setup.txt";s:4:"1c8b";s:31:"static/css_styled/constants.txt";s:4:"d0ba";s:27:"static/css_styled/setup.txt";s:4:"862a";}',
+	'version' => '2.5.1',
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"9a09";s:21:"ext_conf_template.txt";s:4:"3a54";s:12:"ext_icon.gif";s:4:"ad8f";s:17:"ext_localconf.php";s:4:"9c0a";s:14:"ext_tables.php";s:4:"bdeb";s:14:"ext_tables.sql";s:4:"dc58";s:13:"locallang.xml";s:4:"f26f";s:16:"locallang_db.xml";s:4:"cd07";s:7:"tca.php";s:4:"aad1";s:37:"pi1/class.tx_srfeuserregister_pi1.php";s:4:"4a11";s:48:"pi1/class.tx_srfeuserregister_pi1_adodb_time.php";s:4:"cdb8";s:50:"pi1/class.tx_srfeuserregister_pi1_urlvalidator.php";s:4:"b2a7";s:18:"pi1/ext_tables.php";s:4:"b0ed";s:23:"pi1/flexform_ds_pi1.xml";s:4:"bc95";s:19:"pi1/icon_delete.gif";s:4:"f914";s:21:"pi1/internal_link.gif";s:4:"12b9";s:32:"pi1/internal_link_new_window.gif";s:4:"402a";s:17:"pi1/locallang.xml";s:4:"6bd8";s:36:"pi1/tx_srfeuserregister_htmlmail.css";s:4:"0570";s:42:"pi1/tx_srfeuserregister_htmlmail_xhtml.css";s:4:"f65b";s:41:"pi1/tx_srfeuserregister_pi1_css_tmpl.html";s:4:"044a";s:38:"pi1/tx_srfeuserregister_pi1_sample.txt";s:4:"297e";s:37:"pi1/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"95d3";s:14:"doc/manual.sxw";s:4:"34e3";s:48:"hooks/class.tx_srfeuserregister_hooksHandler.php";s:4:"3d7e";s:28:"scripts/jsfunc.updateform.js";s:4:"e838";s:30:"static/old_style/constants.txt";s:4:"4bde";s:30:"static/old_style/editorcfg.txt";s:4:"cfad";s:26:"static/old_style/setup.txt";s:4:"1c8b";s:31:"static/css_styled/constants.txt";s:4:"d0ba";s:27:"static/css_styled/setup.txt";s:4:"862a";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
