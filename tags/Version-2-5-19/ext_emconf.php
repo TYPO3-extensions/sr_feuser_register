454dc2cdbab2e5da9ecc939dd5c03c374e8d6cd6
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-24 21:23:29 +0000

    version 2.5.19 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register@12288 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tags/Version-2-5-19/ext_emconf.php
@@ -0,0 +1,56 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "sr_feuser_register"
+#
+# Auto generated 24-09-2008 23:17
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Frontend User Registration',
+	'description' => 'A self-registration variant of Kasper Skårhøj\'s Front End User Admin extension.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'dependencies' => 'cms,static_info_tables,div2007',
+	'conflicts' => 'germandates,rlmp_language_detection',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => 0,
+	'uploadfolder' => 1,
+	'createDirs' => 'uploads/tx_srfeuserregister',
+	'modify_tables' => 'fe_users',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Stanislas Rolland / Franz Holzinger',
+	'author_email' => 'contact@fholzinger.com',
+	'author_company' => 'jambage.com',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '2.5.19',
+	'_md5_values_when_last_written' => 'a:53:{s:9:"ChangeLog";s:4:"73f2";s:16:"contributors.txt";s:4:"c275";s:21:"ext_conf_template.txt";s:4:"f9c5";s:12:"ext_icon.gif";s:4:"ad8f";s:17:"ext_localconf.php";s:4:"a76d";s:14:"ext_tables.php";s:4:"80f4";s:14:"ext_tables.sql";s:4:"545a";s:13:"locallang.xml";s:4:"f26f";s:16:"locallang_db.xml";s:4:"ec00";s:7:"tca.php";s:4:"aad1";s:14:"doc/manual.sxw";s:4:"4dad";s:38:"lib/class.tx_srfeuserregister_auth.php";s:4:"8b22";s:39:"lib/class.tx_srfeuserregister_email.php";s:4:"ebab";s:38:"lib/class.tx_srfeuserregister_lang.php";s:4:"aa05";s:44:"lib/class.tx_srfeuserregister_lib_tables.php";s:4:"6664";s:45:"lib/class.tx_srfeuserregister_passwordmd5.php";s:4:"c414";s:37:"lib/class.tx_srfeuserregister_tca.php";s:4:"e2af";s:37:"pi1/class.tx_srfeuserregister_pi1.php";s:4:"ad1c";s:48:"pi1/class.tx_srfeuserregister_pi1_adodb_time.php";s:4:"a7cd";s:42:"pi1/class.tx_srfeuserregister_pi1_base.php";s:4:"a8ca";s:50:"pi1/class.tx_srfeuserregister_pi1_urlvalidator.php";s:4:"b79f";s:23:"pi1/flexform_ds_pi1.xml";s:4:"abb2";s:19:"pi1/icon_delete.gif";s:4:"f914";s:21:"pi1/internal_link.gif";s:4:"12b9";s:32:"pi1/internal_link_new_window.gif";s:4:"402a";s:17:"pi1/locallang.xml";s:4:"324e";s:36:"pi1/tx_srfeuserregister_htmlmail.css";s:4:"0570";s:42:"pi1/tx_srfeuserregister_htmlmail_xhtml.css";s:4:"f65b";s:41:"pi1/tx_srfeuserregister_pi1_css_tmpl.html";s:4:"1126";s:38:"pi1/tx_srfeuserregister_pi1_sample.txt";s:4:"297e";s:37:"pi1/tx_srfeuserregister_pi1_tmpl.tmpl";s:4:"da07";s:42:"view/class.tx_srfeuserregister_display.php";s:4:"f1bc";s:45:"control/class.tx_srfeuserregister_control.php";s:4:"06c6";s:50:"control/class.tx_srfeuserregister_control_main.php";s:4:"22af";s:46:"control/class.tx_srfeuserregister_setfixed.php";s:4:"a415";s:48:"hooks/class.tx_srfeuserregister_hooksHandler.php";s:4:"4211";s:45:"hooks/class.tx_srfeuserregister_hooks_cms.php";s:4:"0f79";s:47:"model/class.tx_srfeuserregister_controldata.php";s:4:"7838";s:40:"model/class.tx_srfeuserregister_data.php";s:4:"4e2c";s:46:"model/class.tx_srfeuserregister_model_conf.php";s:4:"9f42";s:49:"model/class.tx_srfeuserregister_model_feusers.php";s:4:"d143";s:50:"model/class.tx_srfeuserregister_model_setfixed.php";s:4:"08e4";s:52:"model/class.tx_srfeuserregister_model_table_base.php";s:4:"9afd";s:39:"model/class.tx_srfeuserregister_url.php";s:4:"207b";s:58:"model/field/class.tx_srfeuserregister_model_field_base.php";s:4:"be25";s:63:"model/field/class.tx_srfeuserregister_model_field_usergroup.php";s:4:"0a88";s:28:"scripts/jsfunc.updateform.js";s:4:"3552";s:43:"marker/class.tx_srfeuserregister_marker.php";s:4:"c6f0";s:31:"static/css_styled/constants.txt";s:4:"ed98";s:27:"static/css_styled/setup.txt";s:4:"cfb2";s:30:"static/old_style/constants.txt";s:4:"c352";s:30:"static/old_style/editorcfg.txt";s:4:"cfad";s:26:"static/old_style/setup.txt";s:4:"c842";}',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'static_info_tables' => '2.0.5-',
+			'php' => '4.2.0-0.0.0',
+			'typo3' => '4.0.0-0.0.0',
+			'div2007' => '0.1.14-',
+		),
+		'conflicts' => array(
+			'germandates' => '0.0.0-1.0.1',
+			'rlmp_language_detection' => '0.0.0-1.2.99',
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ tags/Version-2-5-19/pi1/class.tx_srfeuserregister_pi1_base.php
@@ -0,0 +1,70 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 1999-2003 Kasper Skårhøj <kasperYYYY@typo3.com>
+*  (c) 2004-2008 Stanislas Rolland <stanislas.rolland(arobas)sjbr.ca)>
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ *
+ * Front End creating/editing/deleting records authenticated by fe_user login.
+ * A variant restricted to front end user self-registration and profile maintenance, with a number of enhancements (see the manual).
+ *
+ * $Id$
+ * 
+ * @author	Kasper Skårhøj <kasperYYYY@typo3.com>
+ * @author	Stanislas Rolland <stanislas.rolland(arobas)sjbr.ca>
+ * @author	Franz Holzinger <contact@fholzinger.com>
+ * @maintainer	Franz Holzinger <contact@fholzinger.com> 
+ *
+ *
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(PATH_BE_srfeuserregister.'control/class.tx_srfeuserregister_control_main.php');
+
+
+class tx_srfeuserregister_pi1_base extends tslib_pibase {
+
+		// Plugin initialization variables
+	var $prefixId = 'tx_srfeuserregister_pi1';		// Should be same as classname of the plugin, used for CSS classes, variables
+	var $scriptRelPath = 'pi1/class.tx_srfeuserregister_pi1_base.php'; // Path to this script relative to the extension dir.
+	var $extKey = SR_FEUSER_REGISTER_EXTkey;		// Extension key.
+
+	function main($content, &$conf) {
+		global $TSFE;
+
+		$this->pi_setPiVarDefaults();
+		$this->conf = &$conf;
+
+		$mainObj = &t3lib_div::getUserObj('&tx_srfeuserregister_control_main');
+		$mainObj->cObj = &$this->cObj;
+		$content = &$mainObj->main($content, $conf, $this, 'fe_users');
+		return $content;
+	}
+}
+
+
+if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/sr_feuser_register/pi1/class.tx_srfeuserregister_pi1_base.php']) {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/sr_feuser_register/pi1/class.tx_srfeuserregister_pi1_base.php']);
+}
+?>
