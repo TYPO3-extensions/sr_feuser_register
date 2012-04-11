44541c2c6e40407c531d4d1ce9786de25c2219d7
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-11 23:59:00 +0000

    Add feature #35881: Add a status report about the installation of the extension (TYPO3 4.6+ only)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register@60575 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_autoload.php
+++ trunk/ext_autoload.php
@@ -6,6 +6,7 @@
  */
 $sr_feuser_registerExtensionPath = t3lib_extMgm::extPath('sr_feuser_register');
 return array(
+	'tx_srfeuserregister_statusreport' => $sr_feuser_registerExtensionPath . 'hooks/statusreport/class.tx_srfeuserregister_statusReport.php',
 	'tx_srfeuserregister_pi1' => $sr_feuser_registerExtensionPath . 'pi1/class.tx_srfeuserregister_pi1.php',
 	'tx_srfeuserregister_pi1_base' => $sr_feuser_registerExtensionPath . 'pi1/class.tx_srfeuserregister_pi1_base.php',
 	'tx_srfeuserregister_pi1_urlvalidator' => $sr_feuser_registerExtensionPath . 'pi1/class.tx_srfeuserregister_pi1_urlvalidator.php',
