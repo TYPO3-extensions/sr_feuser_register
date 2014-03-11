38ec383ae392c6bec88bca5a25e82481b0aa8a6e
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-11 22:10:25 +0000

    Fix PHP warning in tx_srfeuserregister_select_dmcategories
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register/trunk@84218 735d13b6-9817-0410-8766-e36946ffe9aa

--- scripts/class.tx_srfeuserregister_select_dmcategories.php
+++ scripts/class.tx_srfeuserregister_select_dmcategories.php
@@ -2,7 +2,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2013 Stanislas Rolland <typo3(arobas)sjbr.ca>
+ *  (c) 2013-2014 Stanislas Rolland <typo3(arobas)sjbr.ca>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -63,7 +63,6 @@ class tx_srfeuserregister_select_dmcategories {
 	 * @return	void		...
 	 */
 	public function get_localized_categories ($params) {
-		global $TCA, $TYPO3_DB, $LANG;
 
 /*
 		$params['items'] = &$items;
@@ -79,29 +78,29 @@ class tx_srfeuserregister_select_dmcategories {
 		$table = $config['itemsProcFunc_config']['table'];
 
 			// initialize backend user language
-		if ($LANG->lang && t3lib_extMgm::isLoaded('static_info_tables')) {
-			$res = $TYPO3_DB->exec_SELECTquery(
+		if ($GLOBALS['LANG']->lang && t3lib_extMgm::isLoaded('static_info_tables')) {
+			$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
 				//'sys_language.uid,static_languages.lg_collate_locale',
 				'sys_language.uid',
 				'sys_language LEFT JOIN static_languages ON sys_language.static_lang_isocode=static_languages.uid',
-				'static_languages.lg_typo3='.$TYPO3_DB->fullQuoteStr($LANG->lang,'static_languages').
-					t3lib_pageSelect::enableFields('sys_language').
-					t3lib_pageSelect::enableFields('static_languages')
+				'static_languages.lg_typo3=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($GLOBALS['LANG']->lang,'static_languages')
+					. t3lib_pageSelect::enableFields('sys_language')
+					. t3lib_pageSelect::enableFields('static_languages')
 				);
-			while($row = $TYPO3_DB->sql_fetch_assoc($res)) {
+			while($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
 				$this->sys_language_uid = $row['uid'];
 				$this->collate_locale = $row['lg_collate_locale'];
 			}
 		}
-		reset($params['items']);
-
-		while(list($k, $item) = each($params['items'])) {
-				$res = $TYPO3_DB->exec_SELECTquery(
+		if (is_array($params['items'])) {
+			reset($params['items']);
+			while(list($k, $item) = each($params['items'])) {
+				$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
 					'*',
 					$table,
 					'uid='.intval($item[1])
 					);
-				while($rowCat = $TYPO3_DB->sql_fetch_assoc($res)) {
+				while($rowCat = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
 					$localizedRowCat =
 						tx_srfeuserregister_dmstatic::getRecordOverlay(
 							$table,
@@ -113,13 +112,11 @@ class tx_srfeuserregister_select_dmcategories {
 						$params['items'][$k][0] = $localizedRowCat['category'];
 					}
 				}
+			}
 		}
-
 	}
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sr_feuser_register/scripts/class.tx_srfeuserregister_select_dmcategories.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sr_feuser_register/scripts/class.tx_srfeuserregister_select_dmcategories.php']);
 }
-
-?>
