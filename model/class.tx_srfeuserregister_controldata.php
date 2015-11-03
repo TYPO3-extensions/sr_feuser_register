be38d1d0fcc5a3ade2e90b61cddbf4cc1b44cd46
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-11-03 15:51:04 +0000

    Adjust copyright notices
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_feuser_register/trunk@90183 735d13b6-9817-0410-8766-e36946ffe9aa

--- model/class.tx_srfeuserregister_controldata.php
+++ model/class.tx_srfeuserregister_controldata.php
@@ -1,38 +1,32 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2007-2012 Franz Holzinger (franz@ttproducts.de)
-*  (c) 2012-2015 Stanislas Rolland <typo3(arobas)sjbr.ca>
-*  All rights reserved
-*
-*  This script is part of the Typo3 project. The Typo3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+
+/*
+ *  Copyright notice
+ *
+ *  (c) 2007-2012 Franz Holzinger <franz@ttproducts.de>
+ *  (c) 2012-2015 Stanislas Rolland <typo3(arobas)sjbr.ca>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
+
 /**
  * Control data store functions
  */
-
 define ('MODE_NORMAL', '0');
 define ('MODE_PREVIEW', '1');
-
-
 class tx_srfeuserregister_controldata {
 	public $thePid = 0;
 	public $thePidTitle;
--- model/field/class.tx_srfeuserregister_model_field_base.php
+++ model/field/class.tx_srfeuserregister_model_field_base.php
@@ -1,44 +1,30 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2008-2011 Franz Holzinger (franz@ttproducts.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
-/**
- * Part of the sr_feuser_register (Frontend User Registration) extension.
+
+/*
+ *  Copyright notice
  *
- * base class for all database table fields classes
+ *  (c) 2008-2011 Franz Holzinger <franz@ttproducts.de>
+ *  (c) 2012-2015 Stanislas Rolland <typo3(arobas)sjbr.ca>
+ *  All rights reserved
  *
- * $Id$
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * @author	Franz Holzinger <franz@ttproducts.de>
- * @maintainer	Franz Holzinger <franz@ttproducts.de>
- * @package TYPO3
- * @subpackage sr_feuser_register
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  GNU General Public License for more details.
  *
+ *  This copyright notice MUST APPEAR in all copies of the script!
  */
 
-
+/**
+ * Base class for all database table fields classes
+ */
 class tx_srfeuserregister_model_field_base  {
 	public $bHasBeenInitialised = FALSE;
 
