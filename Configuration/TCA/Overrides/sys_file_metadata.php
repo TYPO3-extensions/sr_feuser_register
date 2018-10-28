3da3ddd6ff82eba60d9eef62896cb87d7c91b34e
Stanislas Rolland <typo3@sjbr.ca>
2018-10-28 19:39:19 -0400

    [BUGFIX] Error during installation when filemetadata is not installed
    TCA override should check that extension filemetadata is installed.
    
    Resolves: #86784
    Change-Id: Ie980ea8c5e7b9eebc9ec9c85df6ab4212ea17edd
    Reviewed-on: https://review.typo3.org/58773
    Reviewed-by: Stanislas Rolland <typo3@sjbr.ca>
    Tested-by: Stanislas Rolland <typo3@sjbr.ca>

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -1,4 +1,5 @@
 <?php
 defined('TYPO3_MODE') or die();
-
-$GLOBALS['TCA']['sys_file_metadata']['columns']['fe_groups']['config']['foreign_table_where'] = ' AND fe_groups.sys_language_uid IN (-1,0) ORDER BY fe_groups.title';
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('filemetadata')) {
+    $GLOBALS['TCA']['sys_file_metadata']['columns']['fe_groups']['config']['foreign_table_where'] = ' AND fe_groups.sys_language_uid IN (-1,0) ORDER BY fe_groups.title';
+}
